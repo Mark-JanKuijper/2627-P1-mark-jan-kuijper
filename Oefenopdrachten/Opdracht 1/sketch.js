c581ad1d054d0cff2168e87d35b0c74dad21ed76
Mark-JanKuijper <buurman01@outlook.com>
2026-09-08 13:59:38 +0200

    oefen opdract 1
    goed genoeg, oefen opdracht is voor nu klaar

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,35 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(235);
+  fill(255)
+  strokeWeight(10)
+  rect(0,0,600,400)
+ //backround is a square
+ 
+ { strokeWeight(4);
+ fill(255,0,0);
+ square(20,50,100); }
+ //rood
+ fill(0,0,255);
+  square(13,270,100); 
+ //blauw
+ fill (0)
+ square(300,270,100);
+ //zwart
+ fill(255,255,0);
+ square(453,22,100); 
+ //geel
+ fill(0,0,0)
+ square(160,27,100); 
+ //zwart
+ fill(255,0,0);
+ square(129,150,100); 
+ //rood
+ fill(0,0,255);
+ square(480,157,100); 
+ //blauw
+ 
 }
