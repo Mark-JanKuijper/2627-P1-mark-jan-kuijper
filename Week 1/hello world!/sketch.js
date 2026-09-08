9b2d21d66d5ab7ba21c78fe3afcf3579103166dc
Mark-JanKuijper <buurman01@outlook.com>
2026-09-08 16:22:54 +0200

    begining with mario
    stop sign dice and the begining of mario is here

--- Week 1/hello world!/sketch.js
+++ Week 1/hello world!/sketch.js
@@ -1,9 +1,10 @@
 function setup() {
-  createCanvas(800, 1000);
+  createCanvas(1000, 1000);
 
 }
 
 function draw() {
+  stroke(1);
   background(220);
  textSize(32)
  fill(0)
@@ -39,5 +40,146 @@ function draw() {
  triangle(25, 480, 90, 440, 150, 480)
  //huis hierboven
  
+ fill(200)
+ rect(280, 300, 50, -200);
+ rect(292.5, 300, 25, 60);
+ fill(255,0,0);
+ ellipse(305,120,30,30);
+ fill(235,145,0);
+ ellipse(305,195,30,30);
+ fill(0,255,0);
+ ellipse(305,280,30,30);
+ //boven hier stoplicht
+
+
+ 
+
+ fill(255);
+ square(250, 425, 65);
+ fill(0);
+ circle(282,456,25);
+ //boven hier een dobbelsteen
+
+
+
+ fill('red');
+ square(600,100,25);
+ square(575,100,25);
+ square(550,100,25);
+ square(525,100,25);
+ square(500,100,25);
+ square(475,100,25);
+ square(475,125,25);
+ square(450,125,25);
+ square(500,125,25);
+ square(525,125,25);
+ square(550,125,25);
+ square(475,125,25);
+ square(450,125,25);
+ square(575,125,25);
+ square(600,125,25);
+ square(625,125,25);
+ square(650,125,25);
+ square(675,125,25);
+
+ fill('#ffe1c2')
+ square(625,150,25);
+ square(575,150,25);
+ square(550,150,25);
+ square(525,150,25);
+ square(625,175,25);
+ square(675,175,25);
+ square(650,175,25);
+ square(675,200,25);
+ square(700,200,25);
+ square(650,200,25);
+ square(500,175,25);
+ square(525,175,25);
+ square(550,175,25);
+ square(575,175,25);
+ square(450,200,25);
+ square(450,175,25);
+ square(525,200,25);
+ square(550,200,25);
+ square(575,200,25);
+ square(600,200,25);
+ square(550,225,25);
+ square(525,225,25);
+ square(500,225,25);
+ square(575,225,25);
+ square(475,225,25);
+ square(550,250,25);
+ square(575,250,25);
+ square(475,250,25);
+ square(500,250,25);
+ square(525,250,25);
+ square(600,250,25);
+ square(625,250,25);
+ square(650,250,25);
+
+ fill('blue')
+ square(500,275,25);
+ square(500,300,25);
+ square(500,325,25);
+ square(525,325,25);
+ square(550,325,25);
+ square(575,325,25);
+ square(575,300,25);
+ square(525,350,25);
+ square(500,375,25);
+ square(475,350,25);
+ square(475,375,25);
+ square(525,375,25);
+ square(550,375,25);
+ square(575,375,25);
+ square(600,375,25);
+ square(600,350,25);
+ square(550,350,25);
+ square(450,400,25);
+ square(475,400,25);
+ square(500,400,25);
+ square(525,400,25);
+ square(550,400,25);
+ square(575,400,25);
+ square(600,400,25);
+ square(625,400,25);
+ square(625,425,25);
+ square(600,425,25);
+ square(575,425,25);
+ square(500,425,25);
+ square(475,425,25);
+ square(450,425,25);
+ 
+ fill('yellow')
+ square(500,350,25);
+ square(575,350,25);
+
+ fill('red')
+ square(575,275,25);
+ square(550,275,25);
+ square(525,275,25);
+ square(525,300,25);
+ square(550,300,25);
+ square(600,275,25);
+ square(600,300,25);
+ square(600,325,25);
+ square(675,325,25);
+ square(650,325,25);
+ square(625,325,25);
+ square(650,300,25);
+ square(625,300,25);
+ square(625,350,25);
+ square(450,350,25);
+ square(450,325,25);
+ square(475,325,25);
+ square(425,325,25);
+ square(400,325,25);
+ square(450,300,25);
+ square(475,300,25);
+ square(425,300,25);
+ square(450,275,25);
+ square(475,275,25);
+
+ // mario
 }
 
