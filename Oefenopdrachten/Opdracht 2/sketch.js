b125706f54eeb12da87dbecdf459b3ff93c8a2a4
Mark-JanKuijper <buurman01@outlook.com>
2026-09-14 11:07:51 +0200

    oefen opdracht 2
    leren met variabelen te werken

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,53 @@
+
+  let x = 100
+ // dit betekend dat alle instantses
+ // met x is 100
 function setup() {
   createCanvas(400, 400);
+
 }
 
 function draw() {
-  background(220);
+  background(210);
+
+  textSize(20)
+  text(x,20,20)
+
+  //important about variables
+  //they can be anything
+
+  let greeting = "Hello world!"
+
+  text(greeting,20,60)
+  //when using variables for text
+  //you dont have to use the "" symbols
+
+  let a = 20
+
+  let b = 10
+
+  let optellen = a + b 
+
+  let aftrekken = a - b
+
+  let vermenigvulden = a * b
+
+  let delen = a / b
+
+  let y = 80
+
+  text("optellen: "+optellen,20,y)
+// you can use + to add text to text
+  y+=20;
+//this does constant movement
+//the reason that its not moving is that the y = 80 is there to makes it not move
+  text("aftrekken: "+aftrekken,20,y)
+
+  y+=20;
+
+  text("vermenigvulden: "+vermenigvulden,20,y)
+
+  y+=20;
+
+  text("delen: "+delen,20,y)
 }
