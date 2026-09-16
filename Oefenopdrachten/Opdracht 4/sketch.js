8badfca3cfd883c19b565484584f6131d5c0ca91
Mark-JanKuijper <buurman01@outlook.com>
2026-09-16 19:21:45 +0200

    oefen opdracht 4 8ball werkt
    8ball now moves not smoothly but good enough

--- Oefenopdrachten/Opdracht 4/sketch.js
+++ Oefenopdrachten/Opdracht 4/sketch.js
@@ -5,6 +5,7 @@ let score = 0;
 let light_color = 'green';
 let movement0 = 0;
 let movement1 = 0;
+// every variables
 
 function draw() {
   background(220);
@@ -14,9 +15,13 @@ function draw() {
   text("Druk op spatie om het getal op 0 te zetten", 20, 120)
   text("Druk op enter om van rood->groen->oranje te gaan.", 20, 240)
   text("beweeg de eightball met wasd of pijltoetsen.", 200, 50)
-
+ 
   if (keyIsPressed == true) {
+    //keyisPressed is important to ensure that keys work right
     if (keyCode === 66) {
+      //small anoyance is that you have to 
+      // look up the number of the key that
+      //  you want to work with
       fill(120)
       square(20, 30, 60)
     }
@@ -27,6 +32,7 @@ function draw() {
     }
 
   }
+  // up here is the square
   fill(0)
   text(score, 60, 150)
   score += 1
@@ -36,7 +42,8 @@ function draw() {
     
     fill(180)
     rect(50, 300, 40, 90)
-    
+    // little reminder to myself is that it is beter to
+    // make smaller lines of code and to take it 1 step at a time
     if(light_color == 'red')
     {
       fill("red")
@@ -46,7 +53,7 @@ function draw() {
       fill("grey");
     }
     circle(70,315,20)
-
+   // this ensures that the light becomes red
 
     
     if(light_color == 'orange')
@@ -58,7 +65,7 @@ function draw() {
       fill("grey");
     }
     circle(70, 345, 20)
-    
+    //here orange
     
     if(light_color == 'green')
      {
@@ -69,13 +76,28 @@ function draw() {
        fill("grey");
      }
   circle(70, 375, 20)
-
+ //here is it green
+  
   fill(0)
-  circle(movement0+300,160,50)
+  circle(movement0+300,movement1+160,50)
   fill(240)
-  circle(movement0+300,160,38)
+  circle(movement0+300,movement1+160,38)
   fill(0)
-  text("8",movement0+297.5,163)
+  text("8",movement0+297.5,movement1+163)
+
+  if(movement0>=450){
+    movement0 =-450
+  }
+  if(movement0<=-460){
+    movement0 = 440
+  }
+  if(movement1>=450){
+    movement1 = -450
+  }
+  if(movement1<=-470){
+    movement1 = 440
+  }
+  //this here is the rules for the 8ball
 }
 
 function keyPressed()
@@ -95,9 +117,23 @@ function keyPressed()
     }
 
   
-  if(keycode === 87){
-    movement0 += 100
-  } else{movement0+2}0
+    //down here is the movement for the 8ball
+  if(keyCode === 87 | keyCode == 38){
+    movement1 += -10
+  }
+  // W^
+  if(keyCode === 83 | keyCode == 40){
+    movement1 += 10
+  }
+  // S^
+  if(keyCode === 65 | keyCode == 37){
+    movement0 += -10
+  }
+  // A^
+  if(keyCode === 68 | keyCode == 39){
+    movement0 += 10
+  }
+  // D^
 }
 
 
