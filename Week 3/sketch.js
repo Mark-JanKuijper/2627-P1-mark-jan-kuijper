6a73cb1208976760960e93c91ae0a083e37c51b6
Mark-JanKuijper <buurman01@outlook.com>
2026-09-21 10:53:36 +0200

    begining of week opdracht 3
    begin with 9 squares
    basic mouse changing colors

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,7 +1,52 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
+
+let state_square = 0;
+let player_turn = 0;
+
+
+
+//variables
 
 function draw() {
   background(220);
+
+  
+ square(100,100,75)
+
+ square(175,100,75)
+
+ square(250,100,75)
+ //eerste rij
+
+ square(100,175,75)
+
+ square(175,175,75)
+
+ square(250,175,75)
+ //tweede rij
+
+ square(100,250,75)
+
+ square(175,250,75)
+
+ square(250,250,75)
+ //derde rij
+
+
+}
+
+function mouseClicked(){
+  if(state_square == 0){
+    fill(180)
+    state_square = 1
+  } else if(state_square == 1){
+    fill(255,10,0)
+    state_square = 2
+  } else if(state_square == 2){
+    fill(180)
+    state_square = 1
+  }
+
 }
