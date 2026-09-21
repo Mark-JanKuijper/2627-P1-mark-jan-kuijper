19a8e775b6afba0514f8cc8c2c878ff6f139bbb1
Mark-JanKuijper <buurman01@outlook.com>
2026-09-21 12:30:27 +0200

    week opdracht 3
    made it so that the squares change colors, but all of them change colors so that needs changing

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -6,13 +6,27 @@ let state_square = 0;
 let player_turn = 0;
 
 
-
 //variables
 
 function draw() {
   background(220);
-
-  
+ 
+  //the code you see underneath here is copied
+  //is from the mouse position en hover help page
+ if(state_square == 1){
+  if(mouseX > 100 && mouseX < 100 + 75 &&
+    mouseY > 100 && mouseY < 100 + 75
+  ){
+    fill(0,255,0)
+  }
+ } else if(state_square == 2){
+  if(mouseX > 100 && mouseX < 100 + 75 &&
+    mouseY > 100 && mouseY < 100 + 75
+  ){
+    fill(255,0,0)
+  }
+ } 
+ 
  square(100,100,75)
 
  square(175,100,75)
@@ -39,13 +53,13 @@ function draw() {
 
 function mouseClicked(){
   if(state_square == 0){
-    fill(180)
+    
     state_square = 1
   } else if(state_square == 1){
-    fill(255,10,0)
+    
     state_square = 2
   } else if(state_square == 2){
-    fill(180)
+    
     state_square = 1
   }
 
