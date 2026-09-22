28a9468d3fcafc3e70b097e23d39a2caaa2cb692
Mark-JanKuijper <buurman01@outlook.com>
2026-09-22 13:29:46 +0200

    week 3 opdracht
    small save where every sqaure its own light is

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -10,7 +10,7 @@ let square2 = 0;
 let square3 = 0;
 let square4 = 0;
 let square5 = 0;
-let sqaure6 = 0;
+let square6 = 0;
 let square7 = 0;
 let square8 = 0; 
 let square9 = 0;
@@ -39,29 +39,30 @@ function draw() {
  square(100,100,75,10)
  fill(255)
 
- if(state_square == 1){
+ if(square2 == 1){
   if(mouseX > 175 + 10 && mouseX < 175 + 10 + 75 &&
     mouseY > 100 && mouseY < 100 + 75
   ){
-    fill(0,255,0)
+    fill(0,100,0)
   }
- } else if(state_square == 2){
+ } else if(square2 == 2){
   if(mouseX > 175 + 10 && mouseX < 175 + 10 + 75 &&
     mouseY > 100 && mouseY < 100 + 75
   ){
-    fill(255,0,0)
+    fill(100,100,0)
   }
  } 
+ 
  square(175 + 10,100,75,10)
+ fill(255)
 
-
- if(state_square == 1){
+ if(square3 == 1){
   if(mouseX > 250 + 20 && mouseX < 250 + 20 + 75 &&
     mouseY > 100 && mouseY < 100 + 75
   ){
     fill(0,255,0)
   }
- } else if(state_square == 2){
+ } else if(square3 == 2){
   if(mouseX > 250 + 20 && mouseX < 250 + 20 + 75 &&
     mouseY > 100 && mouseY < 100 + 75
   ){
@@ -69,106 +70,114 @@ function draw() {
   }
  } 
  square(250 + 20,100,75,10)
-
+fill(255)
 
  //eerste rij
 
-if(state_square == 1){
+if(square4 == 1){
   if(mouseX > 100 && mouseX < 100 + 75 &&
     mouseY > 175 + 10 && mouseY < 175 + 10 + 75
   ){
-    fill(0,255,0)
+    fill(0,0,255)
   }
- } else if(state_square == 2){
+ } else if(square4 == 2){
   if(mouseX > 100 && mouseX < 100 + 75 &&
     mouseY > 175 + 10 && mouseY < 175 + 10 + 75
   ){
-    fill(255,0,0)
+    fill(50,255,0)
   }
  } 
  square(100,175 + 10,75,10)
+fill(255)
 
 
-
- if(state_square == 1){
+ if(square5 == 1){
   if(mouseX > 175 + 10 && mouseX < 175 + 10 + 75 &&
     mouseY > 175 + 10 && mouseY < 175 + 10 + 75
   ){
-    fill(0,255,0)
+    fill(0,255,100)
   }
- } else if(state_square == 2){
+ } else if(square5 == 2){
   if(mouseX > 175 + 10 && mouseX < 175 + 10 + 75 &&
     mouseY > 175 + 10 && mouseY < 175 + 10 + 75
   ){
-    fill(255,0,0)
+    fill(120,0,120)
   }
  } 
 
- 
  square(175 + 10,175 + 10,75,10)
+fill(255)
 
-  if(state_square == 1){
+
+  if(square6 == 1){
   if(mouseX > 250 + 20 && mouseX < 250 + 20 + 75 &&
     mouseY > 175 + 10&& mouseY < 175 + 10 + 75
   ){
     fill(0,255,0)
   }
- } else if(state_square == 2){
+ } else if(square6 == 2){
   if(mouseX > 250 + 20 && mouseX < 250 + 20 +75 &&
     mouseY > 175 + 10 && mouseY < 175 + 10 +75
   ){
     fill(255,0,0)
   }
  } 
+
  square(250 + 20,175 + 10,75,10)
+ fill(255)
  //tweede rij
 
-  if(state_square == 1){
+  if(square7 == 1){
   if(mouseX > 100 && mouseX < 100 + 75 &&
     mouseY > 250 + 20 && mouseY < 250 + 20 + 75
   ){
-    fill(0,255,0)
+    fill(0,255,200)
   }
- } else if(state_square == 2){
+ } else if(square7 == 2){
   if(mouseX > 100 && mouseX < 100 + 75 &&
     mouseY > 250 + 20 && mouseY < 250 + 75 + 20
   ){
-    fill(255,0,0)
+    fill(255,200,0)
   }
  } 
- square(100,250 + 20,75,10)
 
-  if(state_square == 1){
+ square(100,250 + 20,75,10)
+ fill(255)
+
+  if(square8 == 1){
   if(mouseX > 175 + 10 && mouseX < 175 + 10 + 75 &&
     mouseY > 250 + 20 && mouseY < 250 + 20 + 75
   ){
-    fill(0,255,0)
+    fill(110,255,110)
   }
- } else if(state_square == 2){
+ } else if(square8 == 2){
   if(mouseX > 175 + 10 && mouseX < 175 + 75 + 10 &&
     mouseY > 250 + 20&& mouseY < 250 + 20 + 75
   ){
-    fill(255,0,0)
+    fill(100,110,110)
   }
  } 
 
  square(175 + 10,250 + 20,75,10)
+ fill(255)
 
 
-  if(state_square == 1){
+  if(square9 == 1){
   if(mouseX > 250 + 20 && mouseX < 250 + 75 + 20 &&
     mouseY > 250 + 20 && mouseY < 250 + 75 + 20
   ){
-    fill(0,255,0)
+    fill(0,0,0)
   }
- } else if(state_square == 2){
+ } else if(square9 == 2){
   if(mouseX > 250 + 20 && mouseX < 250 + 20 + 75 &&
     mouseY > 250 + 20 && mouseY < 250 + 20 + 75
   ){
-    fill(255,0,0)
+    fill(255,255,255)
   }
  } 
+
  square(250 + 20,250 + 20,75,10)
+ fill(255)
 
  //derde rij
 
@@ -188,14 +197,35 @@ function mouseClicked(){
   }
 
   if(state_square == 0){
-
     square1 = 1
+    square2 = 1
+    square3 = 1
+    square4 = 1
+    square5 = 1
+    square6 = 1
+    square7 = 1
+    square8 = 1
+    square9 = 1
   } else if (state_square == 1){
-
     square1 = 2
+    square2 = 2
+    square3 = 2
+    square4 = 2
+    square5 = 2
+    square6 = 2
+    square7 = 2
+    square8 = 2
+    square9 = 2
   } else if (square1 == 2){
-
     square1 = 1
+    square2 = 1
+    square3 = 1
+    square4 = 1
+    square5 = 1
+    square6 = 1
+    square7 = 1
+    square8 = 1
+    square9 = 1
   }
 
 
