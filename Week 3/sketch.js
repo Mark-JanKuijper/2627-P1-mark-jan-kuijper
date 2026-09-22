2a724cb7463f5fdd7e1838e2e4e02eb4ef0f9fdf
Mark-JanKuijper <buurman01@outlook.com>
2026-09-22 11:08:59 +0200

    week opdracht 3 klein verandering
    forced to make it into a grid and had to change the code to accomodate it.
    not.fun.

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -26,26 +26,140 @@ function draw() {
     fill(255,0,0)
   }
  } 
+ square(100,100,75,10)
  
- square(100,100,75)
 
- square(175,100,75)
+ if(state_square == 1){
+  if(mouseX > 175 + 10 && mouseX < 175 + 10 + 75 &&
+    mouseY > 100 && mouseY < 100 + 75
+  ){
+    fill(0,255,0)
+  }
+ } else if(state_square == 2){
+  if(mouseX > 175 + 10 && mouseX < 175 + 10 + 75 &&
+    mouseY > 100 && mouseY < 100 + 75
+  ){
+    fill(255,0,0)
+  }
+ } 
+ square(175 + 10,100,75,10)
 
- square(250,100,75)
+
+ if(state_square == 1){
+  if(mouseX > 250 + 20 && mouseX < 250 + 20 + 75 &&
+    mouseY > 100 && mouseY < 100 + 75
+  ){
+    fill(0,255,0)
+  }
+ } else if(state_square == 2){
+  if(mouseX > 250 + 20 && mouseX < 250 + 20 + 75 &&
+    mouseY > 100 && mouseY < 100 + 75
+  ){
+    fill(255,0,0)
+  }
+ } 
+ square(250 + 20,100,75,10)
+
+
  //eerste rij
 
- square(100,175,75)
+if(state_square == 1){
+  if(mouseX > 100 && mouseX < 100 + 75 &&
+    mouseY > 175 + 10 && mouseY < 175 + 10 + 75
+  ){
+    fill(0,255,0)
+  }
+ } else if(state_square == 2){
+  if(mouseX > 100 && mouseX < 100 + 75 &&
+    mouseY > 175 + 10 && mouseY < 175 + 10 + 75
+  ){
+    fill(255,0,0)
+  }
+ } 
+ square(100,175 + 10,75,10)
 
- square(175,175,75)
 
- square(250,175,75)
+
+ if(state_square == 1){
+  if(mouseX > 175 + 10 && mouseX < 175 + 10 + 75 &&
+    mouseY > 175 + 10 && mouseY < 175 + 10 + 75
+  ){
+    fill(0,255,0)
+  }
+ } else if(state_square == 2){
+  if(mouseX > 175 + 10 && mouseX < 175 + 10 + 75 &&
+    mouseY > 175 + 10 && mouseY < 175 + 10 + 75
+  ){
+    fill(255,0,0)
+  }
+ } 
+
+ 
+ square(175 + 10,175 + 10,75,10)
+
+  if(state_square == 1){
+  if(mouseX > 250 + 20 && mouseX < 250 + 20 + 75 &&
+    mouseY > 175 + 10&& mouseY < 175 + 10 + 75
+  ){
+    fill(0,255,0)
+  }
+ } else if(state_square == 2){
+  if(mouseX > 250 + 20 && mouseX < 250 + 20 +75 &&
+    mouseY > 175 + 10 && mouseY < 175 + 10 +75
+  ){
+    fill(255,0,0)
+  }
+ } 
+ square(250 + 20,175 + 10,75,10)
  //tweede rij
 
- square(100,250,75)
+  if(state_square == 1){
+  if(mouseX > 100 && mouseX < 100 + 75 &&
+    mouseY > 250 + 20 && mouseY < 250 + 20 + 75
+  ){
+    fill(0,255,0)
+  }
+ } else if(state_square == 2){
+  if(mouseX > 100 && mouseX < 100 + 75 &&
+    mouseY > 250 + 20 && mouseY < 250 + 75 + 20
+  ){
+    fill(255,0,0)
+  }
+ } 
+ square(100,250 + 20,75,10)
 
- square(175,250,75)
+  if(state_square == 1){
+  if(mouseX > 175 + 10 && mouseX < 175 + 10 + 75 &&
+    mouseY > 250 + 20 && mouseY < 250 + 20 + 75
+  ){
+    fill(0,255,0)
+  }
+ } else if(state_square == 2){
+  if(mouseX > 175 + 10 && mouseX < 175 + 75 + 10 &&
+    mouseY > 250 + 20&& mouseY < 250 + 20 + 75
+  ){
+    fill(255,0,0)
+  }
+ } 
 
- square(250,250,75)
+ square(175 + 10,250 + 20,75,10)
+
+
+  if(state_square == 1){
+  if(mouseX > 250 && mouseX < 250 + 75 &&
+    mouseY > 250 && mouseY < 250 + 75
+  ){
+    fill(0,255,0)
+  }
+ } else if(state_square == 2){
+  if(mouseX > 250 + 20 && mouseX < 250 + 20 + 75 &&
+    mouseY > 250 + 20 && mouseY < 250 + 20 + 75
+  ){
+    fill(255,0,0)
+  }
+ } 
+ square(250 + 20,250 + 20,75,10)
+
  //derde rij
 
 
