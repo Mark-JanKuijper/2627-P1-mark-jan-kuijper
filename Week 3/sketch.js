cf6629644e5e9266722f29cea6b3d88360caf675
Mark-JanKuijper <buurman01@outlook.com>
2026-09-24 15:11:58 +0200

    update opdracht 3 lines
    added lines to show off where someone won

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -18,6 +18,15 @@ let square7 = 0;
 let square8 = 0;
 let square9 = 0;
 
+let line1 = "off"
+let line2 = "off"
+let line3 = "off"
+let line4 = "off"
+let line5 = "off"
+let line6 = "off"
+let line7 = "off"
+let line8 = "off"
+
 let red = "#ff0000";
 let blue = "#0400ff";
 //variables
@@ -32,6 +41,8 @@ function draw() {
   strokeWeight(5)
   textSize(30)
   fill(0)
+  
+  
 
   if (win == 1) {
     text(" player 1 wins", 40, 50)
@@ -148,6 +159,27 @@ function draw() {
 
   //derde rij
 
+  if(line1 == "on"){
+    line(100+(75/2),90,100+(75/2),360)
+  } if(line2 == "on"){
+    line(185+(75/2),90,185+(75/2),360) // colums
+  }if(line3 == "on"){
+    line(270+(75/2),90,270+(75/2),360)
+
+
+  }if(line4 == "on"){
+    line(70,100+(75/2),370,100+(75/2))
+  }if(line5 == "on"){
+    line(70,185+(75/2),370,185+(75/2)) // rows
+  }if(line6 == "on"){
+   line(70,270+(75/2),370,270+(75/2))
+
+
+  }if(line7 == "on"){
+    line(90,90,350,350) //diagonaal
+  }if(line8 == "on"){
+    line(350,95,95,350)
+  }
 
 }
 
@@ -336,10 +368,28 @@ function mousePressed() {
     win = 3
     retry = 1
   }
-
     // win
 
+   //this here is for the lines that show of where someone won.
+   if(square1 == 1 && square4 == 1 && square7 ==1 || square1 == 2 && square4 == 2 && square7 == 2){
+    line1 = "on"
+   } if(square2 == 1 && square5 == 1 && square8 ==1 || square2 == 2 && square5 == 2 && square8 == 2){
+    line2 = "on"
+   } if(square3 == 1 && square6 == 1 && square9 ==1 || square3 == 2 && square6 == 2 && square9 == 2){
+    line3 = "on"
 
+  } if(square1 == 1 && square2 == 1 && square3 ==1 || square1 == 2 && square2 == 2 && square3 == 2){
+    line4 = "on"
+  } if(square4 == 1 && square5 == 1 && square6 ==1 || square4 == 2 && square5 == 2 && square6 == 2){
+    line5 = "on"
+  } if(square7 == 1 && square8 == 1 && square9 ==1 || square7 == 2 && square8 == 2 && square9 == 2){
+    line6 = "on"
+  } 
+  if(square1 == 1 && square5 == 1 && square9 ==1 || square1 == 2 && square5 == 2 && square9 == 2){
+    line7 = "on"
+  } if(square3 == 1 && square5 == 1 && square7 ==1 || square3 == 2 && square5 == 2 && square7 == 2){
+    line8 = "on"
+  }
 
 
   //rety button
@@ -357,6 +407,16 @@ function mousePressed() {
       square7 = 0
       square8 = 0
       square9 = 0
+
+      line1 = 0;
+      line2 = 0;
+      line3 = 0;
+      line4 = 0;
+      line5 = 0;
+      line6 = 0;
+      line7 = 0;
+      line8 = 0;
+
       player_turn = 0
       win = 0
       game_state = "playing"
@@ -365,7 +425,7 @@ function mousePressed() {
 
   
     // old notes or important
-    //player turns
+    // player turns
     // make sure it is at the bottom
     // otherwise it wil do stuff you dont want it to do
     // order of things is incredubly important
